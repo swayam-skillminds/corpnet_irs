59a4969822cdd12e49ad500963f0fe3f3475cb7c
swayam-skillminds <swayambhosale@skillminds.in>
2025-05-08 17:07:42 +0530

    Update credential.py

--- credential.py
+++ credential.py
@@ -1,5 +1,14 @@
-username = "nrajput@skillminds.in.corpnet.fullphase2"
-password = "navi@1234"
-security_token = "lBMegvFQAo07grFQUrkZ7Y3g"
-API_TOKEN = "30125c53b7054bd4bec8e23cfc409134"
-PRIVATE_DOMAIN = "skillmindsteam.testinator.com"
+import os
+...
+def get_salesforce_connection(domain='test'):
+    try:
+        sf = Salesforce(
+            username=os.environ.get('nrajput@skillminds.in.corpnet.fullphase2'),
+            password=os.environ.get('navi@1234'),
+            security_token=os.environ.get('lBMegvFQAo07grFQUrkZ7Y3g'),
+            domain=domain
+        )
+        return sf
+    except Exception as e:
+        print(f"Failed to connect to Salesforce: {e}")
+        return None
