134506f0dc55c4c3eb59e0895aa40eeddf3e00da
swayam-skillminds <swayambhosale@skillminds.in>
2025-05-08 16:52:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@
 # Install Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
-apt-get update && apt-get install -y google-chrome-stable
-
+# apt-get update && apt-get install -y google-chrome-stable
+pip install --upgrade pip
+pip install -r requirements.txt
 # Print Chrome version
 google-chrome --version
 
 # Install dependencies for ChromeDriver
-apt-get install -y libglib2.0-0 libnss3 libgconf-2-4 libfontconfig1
+# apt-get install -y libglib2.0-0 libnss3 libgconf-2-4 libfontconfig1
